8566520799d278c0533486568ed5f0d256f33943
HoriaMitrica <horia.mitrica@gmail.com>
2024-06-03 14:30:38 +0300

    added auto scrolling and bigger map

--- client/src/components/Player/index.tsx
+++ client/src/components/Player/index.tsx
@@ -1,61 +1,81 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { IPosition, PlayerProps } from '../../models/player'
 import { TILE_SIZE } from '../../constants/constants';
-export const Player:React.FC<PlayerProps> = ({layers}) => {
+export const Player: React.FC<PlayerProps> = ({ layers }) => {
 
-    const [position, setPosition] = useState<IPosition>({ x: 1, y: 1 });
-    console.log(position);
-    useEffect(() => {
-      window.addEventListener('keydown', handleKeyPress);
-      return () => {
-        window.removeEventListener('keydown', handleKeyPress);
-      };
-    }, [position]);
+  const [position, setPosition] = useState<IPosition>({ x: 1, y: 1 });
 
-    const handleKeyPress = (e: KeyboardEvent) => {
-      let newX = position.x;
-      let newY = position.y;
-  
-      if (e.key === 'w') newY -= 1;
-      if (e.key === 's') newY += 1;
-      if (e.key === 'a') newX -= 1;
-      if (e.key === 'd') newX += 1;
-  
-      if (!checkCollision(newX, newY)) {
-        setPosition({ x: newX, y: newY });
-      }
+  useEffect(() => {
+
+    console.log(position.x * TILE_SIZE + "     " + window.innerWidth)
+
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
     };
-  
-    const checkCollision = (x: number, y: number): boolean => {
-      for (const layer of layers) {
-        if (layer.collider) {
-          for (const tile of layer.tiles) {
-            if (tile.x === x && tile.y === y) {
-              return true;
-            }
+  }, [position]);
+
+  const handleKeyPress = (e: KeyboardEvent) => {
+    let newX = position.x;
+    let newY = position.y;
+
+    if (e.key === 'w') {
+      newY -= 1;
+      if (position.y * TILE_SIZE < window.innerHeight) {
+        window.scrollBy(0, -TILE_SIZE * 4);
+      }
+    }
+    if (e.key === 's') {
+      newY += 1;
+      if (position.y * TILE_SIZE > window.innerHeight * 2 / 3) {
+        window.scrollBy(0, TILE_SIZE * 4);
+      }
+    }
+    if (e.key === 'a') {
+      newX -= 1;
+      if (position.x * TILE_SIZE <window.innerWidth) {
+        window.scrollBy(-TILE_SIZE * 4, 0);
+      }
+    }
+    if (e.key === 'd') {
+      newX += 1;
+      if (position.x * TILE_SIZE > window.innerWidth* 2 / 3) {
+        window.scrollBy(TILE_SIZE * 4, 0);  
+      }
+    }
+
+    if (!checkCollision(newX, newY)) {
+      setPosition({ x: newX, y: newY });
+    }
+  };
+
+  const checkCollision = (x: number, y: number): boolean => {
+    for (const layer of layers) {
+      if (layer.collider) {
+        for (const tile of layer.tiles) {
+          if (tile.x === x && tile.y === y) {
+            return true;
           }
         }
       }
-      return false;
-    };
-  
+    }
+    return false;
+  };
 
-    return (
-        <>
-            <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
-
-                <div
-                    key={0}
-                    style={{
-                        position: 'relative',
-                        left: position.x * TILE_SIZE,
-                        top: position.y * TILE_SIZE,
-                        width: TILE_SIZE,
-                        height: TILE_SIZE,
-                        backgroundColor: 'brown',
-                    }}
-                />
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div
+        key={0}
+        style={{
+          position: 'absolute',
+          left: position.x * TILE_SIZE,
+          top: position.y * TILE_SIZE,
+          width: TILE_SIZE,
+          height: TILE_SIZE,
+          backgroundImage: `url(/src/assets/sprites/rogues.png)`,
+          backgroundPosition: `-${32}px -${32}px`
+        }}
+      />
+    </>
+  )
 }
