cf51f20997ba683379049df2d69701391d3b3302
HoriaMitrica <horia.mitrica@gmail.com>
2024-06-04 13:33:25 +0300

    added player sprites

--- client/src/models/enum.ts
+++ client/src/models/enum.ts
@@ -8,3 +8,29 @@ export enum MapPaths{
     TestRoom="/src/assets/json/map.json",
     Room_002="/src/assets/room_002/map.json",
 }
+
+export enum Class{
+    Bandit="/src/assets/sprites/characters/bandit.png",
+    Desert_Sage="/src/assets/sprites/characters/desert_sage.png",
+    Druid="/src/assets/sprites/characters/druid.png",
+    Dwarf="/src/assets/sprites/characters/dwarf.png",
+    Elf="/src/assets/sprites/characters/elf.png",
+    Female_Knight="/src/assets/sprites/characters/female_knight.png",
+    Female_War_Cleric="/src/assets/sprites/characters/female_war_cleric.png",
+    Female_Winter_Barbarian="/src/assets/sprites/characters/female_winter_barbarian.png",
+    Female_Wizard="/src/assets/sprites/characters/female_wizard.png",
+    Male_Barbarian="/src/assets/sprites/characters/male_barbarian.png",
+    Male_Wizard="/src/assets/sprites/characters/male_wizard.png",
+    Male_Knight="/src/assets/sprites/characters/male_knight.png",
+    Male_Fighter="/src/assets/sprites/characters/male_fighter.png",
+    Male_War_Cleric="/src/assets/sprites/characters/male_war_cleric.png",
+    Male_Winter_Barbarian="/src/assets/sprites/characters/male_winter_barbarian.png",
+    Monk="/src/assets/sprites/characters/monk.png",
+    Priest="/src/assets/sprites/characters/priest.png",
+    Ranger="/src/assets/sprites/characters/ranger.png",
+    Rogue="/src/assets/sprites/characters/rogue.png",
+    Shield_Knight="/src/assets/sprites/characters/shield_knight.png",
+    Swordsman="/src/assets/sprites/characters/swordsman.png",
+    Templar="/src/assets/sprites/characters/templar.png"
+    
+}
